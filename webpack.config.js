90b39fb696a8ae199d4db679304b3ee5c09f7953
Andy Evans <andrewevans0@outlook.com>
2018-11-18 21:36:24 +0000

    added in webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    }
+};
